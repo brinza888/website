dfd14bda900550ea69edc303e74424e3a71b11ec
Brinza Bezrukoff <bezrukoff888@gmail.com>
2020-11-24 00:29:43 +0300

    Added flask-admin with base configuration (INSECURE)

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
+
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 from flask_migrate import Migrate
+
+from flask_login import LoginManager
+
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
 
 from config import Config
 
@@ -9,18 +15,24 @@ from config import Config
 db = SQLAlchemy()
 migrate = Migrate()
 login_manager = LoginManager()
+admin = Admin(name="BrinzaBezrukoff", template_mode="bootstrap3")
 
 
-def create_app():
+def create_app(config=Config):
     app = Flask(__name__,
             static_url_path="/static",
             static_folder="./static")
-    app.config.from_object(Config)
+    app.config.from_object(config)
 
     ## Modules ##
     db.init_app(app)
     migrate.init_app(app, db)
     login_manager.init_app(app)
+    admin.init_app(app)
+
+    ## Admin views ##
+    from app.models import User
+    admin.add_view(ModelView(User, db.session))
 
     ## Blueprints ##
     from app.errors import bp as errors_bp
