5ece04be5a88cc2290a30decf7008ccaf1fe545a
Brinza Bezrukoff <bezrukoff888@gmail.com>
2020-11-23 23:58:30 +0300

    Fixed bug for production (Gunicorn recognize only wsgi.py) Added DEBUG_ env vars for testing server

--- config.py
+++ config.py
@@ -5,7 +5,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 
-class Config:  
+class Config:
+    DEBUG = bool(os.environ.get("DEBUG", False))
+    DEBUG_HOST = os.environ.get("DEBUG_HOST", "localhost")
+    DEBUG_PORT = int(os.environ.get("DEBUG_PORT", 5000))
+
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///" + os.path.join(basedir, "app.db"))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
--- site.py
+++
@@ -1,9 +0,0 @@
-from app import create_app
-from app.tools import *
-
-
-app = create_app()
-
-
-if __name__ == '__main__':
-    app.run(host="192.168.2.43", port=5000, debug=True)
