328c0b5dd0b0953a9de2b06c03f736d8adf8573a
Brinza Bezrukoff <bezrukoff888@gmail.com>
2020-11-29 02:40:00 +0300

    Models for advanced controll

--- app/models.py
+++ app/models.py
@@ -5,25 +5,20 @@ from flask_login import UserMixin
 from app import db, login_manager
 
 
+roles_users = db.Table("roles_users",
+    db.Column("user_id", db.Integer(), db.ForeignKey("user.id")),
+    db.Column("role_id", db.Integer(), db.ForeignKey("role.id"))
+)
+
+
 class User (db.Model, UserMixin):
-    id = db.Column(
-        db.Integer,
-        primary_key=True
-    )
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255))
+    profile_name = db.Column(db.String(255))
+    password = db.Column(db.String(255))
 
-    username = db.Column(
-        db.String(50)
-    )
-
-    profile_name = db.Column(
-        db.String(200),
-        nullable=False
-    )
-
-    password = db.Column(
-        db.String(200),
-        nullable=False
-    )
+    roles = db.relationship("Role", secondary=roles_users,
+        lazy="dynamic", backref="users")
 
     def set_password(self, password):
         self.password = User.hash_password(password)
@@ -34,6 +29,35 @@ class User (db.Model, UserMixin):
     @staticmethod
     def hash_password(password):
         return hashlib.sha256(password.encode("utf-8")).hexdigest()
+
+    def has_permission(self, permission_name):
+        pass
+
+    def has_role(self, role_name):
+        return role in self.roles
+
+    def set_role(self, role):
+
+
+
+permissions_roles = db.Table("permissions_roles",
+    db.Colum("role_id", db.Integer(), db.ForeignKey("role.id")),
+    db.Column("permission_id", db.Integer(), db.ForeignKey("permission.id"))
+)
+
+
+class Role (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), unique=True)
+    display_name = db.Column(db.String(255))
+    permissions = db.relationship("Permission", secondary=permissions_roles,
+        lazy="dynamic", backref="roles")
+
+
+class Permission (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(255))
 
 
 @login_manager.user_loader
