b921626a0012a187d1aa98951437d558c7ee02e4
Brinza Bezrukoff <bezrukoff888@gmail.com>
2021-07-27 03:51:39 +0300

    Added timestamps to Release and Project models

--- app/projects/models.py
+++ app/projects/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from app import db
 
 
@@ -6,6 +8,7 @@ class Release (db.Model):
     version = db.Column(db.String(20))
     description = db.Column(db.Text)
     file = db.relationship('File', uselist=False)
+    date = db.Column(db.DateTime, default=datetime.now())
 
     project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
     file_id = db.Column(db.Integer, db.ForeignKey('file.id'))
@@ -15,15 +18,24 @@ class Release (db.Model):
         self.description = description
         self.file = file
 
+    @property
+    def released_date(self):
+        return self.started.strftime("%d.%m.%Y %H:%M")
+
 
 class Project (db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150))
     description = db.Column(db.Text)
     github = db.Column(db.String(1000), nullable=True)
+    started = db.Column(db.DateTime, default=datetime.now())
 
     releases = db.relationship(Release, lazy='dynamic', backref='project')
 
     def __init__(self, name, description):
         self.name = name
         self.description = description
+
+    @property
+    def started_date(self):
+        return self.started.strftime("%d.%m.%Y")
