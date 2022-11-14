dab8ca2c55136ed3ac9a530a9daff188f25ed2da
Ilya Bezrukov <bezrukoff888@gmail.com>
2022-11-15 00:24:01 +0300

    Added HTML input sanitizing using bleach module

--- app/projects/admin_views.py
+++ app/projects/admin_views.py
@@ -1,4 +1,6 @@
 from flask_ckeditor import CKEditorField
+from bleach import clean
+
 
 from app.admin_views import ProtectedModelView
 
@@ -10,3 +12,20 @@ class ProjectAdminView (ProtectedModelView):
         "description": CKEditorField
     }
     column_exclude_list = ["description"]
+
+    def on_model_change(self, form, model, is_created):
+        model.description = clean(model.description, tags=[
+            "h1", "h2", "h3", "h4", "h5", "h6",
+            "a", "p", "li", "ol", "ul", "img",
+            "pre", "code", "i", "b", "s", "em", "strong",
+            "span", "div", "table", "tr", "td", "th",
+            "thead", "tbody", "caption", "blockquote",
+            "br", "hr", "sub", "sup"
+        ], attributes=[
+            "style", "class", "border", "cellpadding", "cellspacing", "summary",
+            "href", "src", "alt", "title", "id", "name"
+        ], styles=[
+            "color", "font-family", "font-weight", "width", "height", "text-align",
+            "font-size"
+        ])
+
